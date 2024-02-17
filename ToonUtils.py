ae22899a0109ef3aeb94106e9fdefe3d2edaf271
Ryan David Ward <ryandward@gmail.com>
2024-02-17 13:44:30 -0600

    Refactor ToonUtils class and add DBConnectionManager

--- ToonUtils.py
+++ ToonUtils.py
@@ -1,9 +1,9 @@
 from AutoCompletion import AutoCompletion
 from Buttons import DMButton, DeleteButton
+from DBConnectionManager import DBConnectionManager
 from RichLogger import RichLogger
 from config import pgdata, pghost, pgpass, pguser
 import logging
-
 import discord
 from discord.ext import commands
 from sqlalchemy import select
@@ -24,50 +24,22 @@ logger = RichLogger(__name__)
 class ToonUtils(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        # Initialize the async engine
-        self.engine = create_async_engine(
-            f"postgresql+asyncpg://{pguser}:{pgpass}@{pghost}:5432/{pgdata}",
-            echo=False,
+        self.db_manager = DBConnectionManager(
+            db_url=f"postgresql+asyncpg://{pguser}:{pgpass}@{pghost}:5432/{pgdata}",
+            search_column="name",
         )
-        # Store the async session factory
-        self.AsyncSession = sessionmaker(
-            bind=self.engine, class_=AsyncSession, expire_on_commit=False
-        )
+        asyncio.create_task(self.db_manager.prepare_tables())
 
-        # Initialize cache placeholder
-        self.auto_completer = None
-
-        # Asynchronously prepare the database reflection
-        asyncio.create_task(self.prepare_tables())
-
-    async def prepare_tables(self):
-        # Asynchronous table reflection
-        async with self.engine.begin() as conn:
-            Base = automap_base()
-            await conn.run_sync(Base.prepare)
-            self.tables = defaultdict()
-            for table_name in Base.classes.keys():
-                self.tables[table_name] = Base.classes[table_name]
-
-            # Initialize AutoCompletion after tables are prepared
-            self.initialize_auto_completer()
-
-    def initialize_auto_completer(self):
-        # Ensure tables are prepared before initializing
-        if self.tables:
-            self.auto_completer = AutoCompletion(
-                async_session_factory=self.AsyncSession,
-                table=self.tables["census"],
-                choice_transformer=lambda row: discord.app_commands.Choice(
-                    name=row[0], value=row[0]
-                ),
-                search_column="name",
-            )
+    def create_cog_view(self, user_id):
+        view = discord.ui.View(timeout=None)
+        view.add_item(DeleteButton(user_id))
+        view.add_item(DMButton(user_id))
+        return view
 
     async def toon_autocomplete(self, interaction: discord.Interaction, current: str):
-        # Ensure auto_completer is initialized
-        if self.auto_completer:
-            return await self.auto_completer.autocomplete(current)
+        auto_completer = await self.db_manager.get_auto_completer("census")
+        if auto_completer:
+            return await auto_completer.autocomplete(current)
         else:
             logger.info("AutoCompletion is not yet initialized.")
             return []
@@ -84,19 +56,23 @@ class ToonUtils(commands.Cog):
     )
     @discord.app_commands.describe(toon="The member to get info about.")
     @discord.app_commands.autocomplete(toon=toon_autocomplete)
-    async def toons(
-        self,
-        interaction: discord.Interaction,
-        toon: str,
-    ):
-        async with self.AsyncSession() as session:
-            stmt = select(self.tables["census"].discord_id).where(
-                self.tables["census"].name == toon
+    async def toons(self, interaction: discord.Interaction, toon: str):
+        census_table = self.db_manager.get_table("census")
+        if not census_table:
+            # Handle error: table not found
+            await interaction.response.send_message(
+                "Error: Census table not found.", ephemeral=True
+            )
+            return
+        
+        async with self.db_manager.AsyncSession() as session:
+            stmt = select(census_table.discord_id).where(
+                census_table.name == toon
             )
             results = await session.execute(stmt)
             discord_id = results.scalars().one()
-            stmt = select(self.tables["census"]).where(
-                self.tables["census"].discord_id == discord_id
+            stmt = select(census_table).where(
+                census_table.discord_id == discord_id
             )
             results = await session.execute(stmt)
             matching_data = results.scalars().all()
