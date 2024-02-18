364334b21dbea39ff35914e145d82424b37457cd
Ryan David Ward <ryandward@gmail.com>
2024-02-17 22:44:42 -0600

    Refactor AutoCompletion class and add ChoiceTransformer class

--- AutoCompletion.py
+++ AutoCompletion.py
@@ -20,7 +20,7 @@ class AutoCompletion:
         self.search_column = search_column
         self.max_choices = max_choices
         logger.info(
-            f"AutoCompletion initialized with search column '{self.search_column}' and max choices {self.max_choices}"
+            f"AutoCompletion initialized. Keyword: '{self.search_column}', max choices: {self.max_choices}"
         )
 
     async def autocomplete(self, current: str):
@@ -29,22 +29,7 @@ class AutoCompletion:
             return []
 
         logger.info(f"Autocomplete requested for '{current}'. Querying database.")
-        async with self.AsyncSession() as session:
-            stmt = (
-                select(
-                    getattr(self.table, self.search_column),
-                    func.count(getattr(self.table, self.search_column)).label(
-                        "quantity"
-                    ),
-                )
-                .where(getattr(self.table, self.search_column).ilike(f"%{current}%"))
-                .group_by(getattr(self.table, self.search_column))
-            )
-
-            logger.info(f"Executing query: {stmt}")
-            results = await session.execute(stmt)
-            choices = results.all()
-            logger.info(f"Query returned {len(choices)} results.")
+        choices = await self._query_database(current)
 
         if not choices:
             logger.info("No matches found.")
@@ -57,3 +42,24 @@ class AutoCompletion:
         logger.info(f"Transformed {len(transformed_choices)} choices.")
 
         return transformed_choices[: self.max_choices]
+
+    async def _query_database(self, current: str):
+        async with self.AsyncSession() as session:
+            stmt = self._create_query(current)
+            logger.info(f"Executing query: {stmt}")
+            results = await session.execute(stmt)
+            choices = results.all()
+            logger.info(f"Query returned {len(choices)} results.")
+        return choices
+
+    def _create_query(self, current: str):
+        return (
+            select(
+                getattr(self.table, self.search_column),
+                func.count(getattr(self.table, self.search_column)).label(
+                    "quantity"
+                ),
+            )
+            .where(getattr(self.table, self.search_column).ilike(f"%{current}%"))
+            .group_by(getattr(self.table, self.search_column))
+        )
